bc5089c418474cfff6c609003786dccbfbc5b3cf
key-seven <151981885+key-seven@users.noreply.github.com>
2023-12-04 09:21:35 +0500

    привет

---
+++ vanya gey.cs
@@ -0,0 +1,17 @@
+﻿int i = 0, z = 1;
+
+int[] array = new int[100];
+
+while ((z = int.Parse(Console.ReadLine())) != 0)
+{
+    array[i] = Convert.ToInt32(z);
+}
+for (int k = 0; k < i; k++)
+{
+    Console.WriteLine(array[k]);
+}
+int max, min;
+max = array.Max();
+min = array.Min();
+
+Console.WriteLine("Максимальное значение: " + max + "\nМинимальное значение: " + min);
